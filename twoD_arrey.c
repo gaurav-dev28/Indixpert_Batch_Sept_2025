7a9035e0d986a9811c443de6eed43a3835b86c46
gaurav-dev28 <gauravdev1998@gmail.com>
2025-10-10 12:43:19 +0530

    First C program for 2D arrey

---
+++ twoD_arrey.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int data[4][4];
+
+    printf("Please enter you data (matrix) in Rows & Coloumn (3*3): \n");
+    for(int y=0; y<4; y++)
+    {
+        for(int z=0; z<4; z++)
+        {
+            scanf("%d",&data[y][z]);
+        }
+    }
+
+    printf("Your Data Matrix: ");
+
+    for(int y=0; y<4; y++)
+    {
+        printf("\n\n");
+        for(int z=0; z<4; z++)
+        {
+            printf(" %d ",data[y][z]);
+        }
+    }
+
+    return 0;
+}
